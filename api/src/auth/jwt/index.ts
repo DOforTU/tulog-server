875d05c7c78a2599bacdd0d502836917bb2996fe
Dohyeon Won <dh1072005@gmail.com>
2025-08-02 01:09:24 +0900

    [DELETE] duplicate guard

--- api/src/auth/jwt/index.ts
+++ api/src/auth/jwt/index.ts
@@ -1,3 +1,3 @@
 export { JwtAuthGuard } from './jwt-auth.guard';
-export { ActiveUserGuard } from './active-user.guard';
+// export { ActiveUserGuard } from './dump/active-user.guard';
 export { SmartAuthGuard } from './smart-auth.guard';
