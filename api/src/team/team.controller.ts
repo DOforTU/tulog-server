405cdd55e8c0678d47966bd3cebee205d6f5c368
leejucheol <161808249+leejucheol@users.noreply.github.com>
2025-08-02 11:45:33 +0900

    Update team.controller.ts

--- api/src/team/team.controller.ts
+++ api/src/team/team.controller.ts
@@ -12,6 +12,7 @@ import { ChangeVisibilityDto, CreateTeamDto } from './team.dto';
 import { TeamService } from './team.service';
 import { Team, TeamVisibility } from './team.entity';
 import { JwtAuthGuard } from 'src/auth/jwt/jwt-auth.guard';
+import { SmartAuthGuard } from 'src/auth/jwt';
 
 @Controller('teams')
 export class TeamController {
@@ -19,6 +20,7 @@ export class TeamController {
 
   // 팀 생성 로직
   @Post()
+  @UseGuards(SmartAuthGuard)
   async createTeam(@Body() teamDto: CreateTeamDto): Promise<boolean> {
     // 팀 생성 로직을 호출
     // 팀 생성 성공 여부를 반환
@@ -27,6 +29,7 @@ export class TeamController {
 
   // 팀 리스트 조회
   @Get()
+  // 조회도 로그인한 사용자만 가능인가?
   async findTeams(): Promise<Team[]> {
     // 팀 리스트 조회
     return await this.teamService.findAllTeams();
@@ -34,6 +37,7 @@ export class TeamController {
 
   // 팀 아이디로 상세 조회 로직
   @Get(':id')
+  @UseGuards(SmartAuthGuard)
   async findTeamById(
     @Param('id', ParseIntPipe) id: string,
   ): Promise<Team | null> {
@@ -43,6 +47,7 @@ export class TeamController {
 
   // 팀 이름으로 상세 조회 로직
   @Get('name/:name')
+  @UseGuards(SmartAuthGuard)
   async findTeamByName(@Param('name') name: string): Promise<Team | null> {
     //
     return await this.teamService.findTeamByName(name);
@@ -50,7 +55,7 @@ export class TeamController {
 
   // 팀 이름 변경 로직
   @Patch(':name')
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(SmartAuthGuard)
   async changeTeamName(
     @Param('name') name: string,
     @Body('newName') newName: string,
@@ -60,6 +65,7 @@ export class TeamController {
 
   // 팀 상태 설정 기능 로직
   @Post(':id/visibility')
+  @UseGuards(SmartAuthGuard)
   async changeStatus(
     @Param('id', ParseIntPipe) id: number,
     @Body() visibility: ChangeVisibilityDto,
