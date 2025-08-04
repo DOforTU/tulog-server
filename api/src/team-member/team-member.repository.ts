ec39e1b23d38547eb8f026b6b347bcfa914facee
leejucheol <161808249+leejucheol@users.noreply.github.com>
2025-08-05 00:32:51 +0900

    update: leave team logic

--- api/src/team-member/team-member.repository.ts
+++ api/src/team-member/team-member.repository.ts
@@ -25,6 +25,14 @@ export class TeamMemberRepository {
       .getMany();
   }
 
+  async findWithTeamsByTeamId(teamId: number): Promise<TeamMember[]> {
+    return await this.teamMemberRepository
+      .createQueryBuilder('teamMember')
+      .leftJoinAndSelect('teamMember.team', 'team')
+      .where('teamMember.teamId = :teamId', { teamId })
+      .getMany();
+  }
+
   async findOneByPrimaryKey(
     memberId: number,
     teamId: number,
@@ -37,5 +45,15 @@ export class TeamMemberRepository {
   async leaveTeam(teamId: number, memberId: number): Promise<boolean> {
     await this.teamMemberRepository.delete({ teamId, memberId });
     return true;
+  }
+
+  async softDeleteTeam(teamId: number) {
+    //await this.teamMemberRepository.softDelete(teamId); 이렇게 작성했는데 맞나? teamId를 기준으로 해당 팀을 삭제
+    // 아래와 같이 쿼리 빌더 사용 -> 근데 우리가 데이터 베이스에서 팀을 softdelete해서 정보는 남겨둔다고 해서 teammember가 해당 teamId에 속한 정보 다 삭제
+    await this.teamMemberRepository
+      .createQueryBuilder()
+      .softDelete()
+      .where('teamId = :teamId', { teamId })
+      .execute();
   }
 }
--- api/src/team-member/team-member.service.ts
+++ api/src/team-member/team-member.service.ts
@@ -1,7 +1,11 @@
 import { UserService } from 'src/user/user.service';
 import { TeamWithStatus } from './team-member.dto';
 import { TeamMemberRepository } from './team-member.repository';
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { TeamMember, TeamMemberStatus } from './team-member.entity';
 
 @Injectable()
@@ -126,13 +130,30 @@ export class TeamMemberService {
    *
    * 먼저 예외 생각하지 않고 팀에서 팀맴버가 삭제되는것만 생각 (팀을 나가는거니까)
    *
-   * teamid로 TeamMember[]를 가져옴 -> TeamMber[]의 count가 1일 때 leaveTeam + Team softDelete
+   * teamid로 TeamMember[]를 가져옴 -> TeamMber[]의 count가 1일 때 leaveTeam + Team softDelete (쿼리 러너 사용)
    * count가 1보다 크면서, 나가는 사람이 리더일 경우
    * throw new exception (`리더 선임을 먼저 하세요`)
    * count가 1보다 크면서, 나가는 사람이 리더가 아닐 경우
    * leaveTeam만 함
    */
   async leaveTeam(teamId: number, memberId: number): Promise<boolean> {
+    const teamMembers =
+      await this.teamMemberRepository.findWithTeamsByTeamId(teamId);
+    if (teamMembers?.length == 1) {
+      await this.teamMemberRepository.leaveTeam(teamId, memberId);
+      await this.teamMemberRepository.softDeleteTeam(teamId);
+      return true;
+    }
+
+    // 나는 왜 아래 로직을 수행해야하는지 잘 이해가 안감 teamId로 해당 맴버를 가져와서 리더인지 판별하는건데 이거를 왜 조건문을 활용하지?
+    const leavingMember = teamMembers.find((m) => m.memberId === memberId);
+    if (!leavingMember) {
+      throw new NotFoundException('해당 멤버가 팀에 존재하지 않습니다.');
+    }
+
+    if (teamMembers?.length > 1 && leavingMember.isLeader === true) {
+      throw new ConflictException(`You must grant to other member`);
+    }
     await this.teamMemberRepository.leaveTeam(teamId, memberId);
     return true;
   }
