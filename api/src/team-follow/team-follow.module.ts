c4865ca813c8ac2d50457e9e1416b9e6274e43ea
leejucheol <161808249+leejucheol@users.noreply.github.com>
2025-08-12 18:13:26 +0900

    Update team-follow.module.ts

--- api/src/team-follow/team-follow.module.ts
+++ api/src/team-follow/team-follow.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TeamFollow } from './team-follow.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TeamFollowService } from './team-follow.service';
+import { TeamFollowRepository } from './team-follow.repository';
+import { TeamFollwController } from './team-follow.controller';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([TeamFollow]), TeamFollowModule],
+  providers: [TeamFollowService, TeamFollowRepository],
+  exports: [TeamFollowService, TeamFollowRepository],
+  controllers: [TeamFollwController],
+})
 export class TeamFollowModule {}
