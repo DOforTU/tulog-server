d2f732377a1beb872026357a0d0f17b7a04004e6
Dohyeon Won <dh1072005@gmail.com>
2025-08-14 14:15:02 +0900

    [FIX] post countLike err

--- api/src/post-like/post-like.service.ts
+++ api/src/post-like/post-like.service.ts
@@ -6,11 +6,15 @@ import {
 import { PostLike } from './post-like.entity';
 import { User } from 'src/user/user.entity';
 import { PostLikeRepository } from './post-like.repository';
-import { EntityManager } from 'typeorm';
+import { DataSource, EntityManager } from 'typeorm';
+import { Post } from 'src/post/post.entity';
 
 @Injectable()
 export class PostLikeService {
-  constructor(private readonly postLikeRepository: PostLikeRepository) {}
+  constructor(
+    private readonly postLikeRepository: PostLikeRepository,
+    private readonly dataSource: DataSource,
+  ) {}
 
   // ===== CREATE =====
 
@@ -22,7 +26,25 @@ export class PostLikeService {
       throw new ConflictException('Post already liked');
     }
 
-    return await this.postLikeRepository.createLike(userId, postId);
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      const postLike = await this.postLikeRepository.createLike(userId, postId);
+      // queryRunner를 활용하여 post의 likeCount 증가
+      await queryRunner.manager
+        .getRepository(Post)
+        .increment({ id: postId }, 'likeCount', 1);
+
+      await queryRunner.commitTransaction();
+      return postLike;
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw error;
+    } finally {
+      await queryRunner.release();
+    }
   }
 
   // ===== READ =====
@@ -52,7 +74,24 @@ export class PostLikeService {
       throw new NotFoundException('Post not liked');
     }
 
-    return await this.postLikeRepository.deleteLike(userId, postId);
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      await queryRunner.manager
+        .getRepository(Post)
+        .decrement({ id: postId }, 'likeCount', 1);
+
+      await this.postLikeRepository.deleteLike(userId, postId);
+      await queryRunner.commitTransaction();
+      return true;
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw error;
+    } finally {
+      await queryRunner.release();
+    }
   }
 
   // ===== SUB FUNCTION =====
