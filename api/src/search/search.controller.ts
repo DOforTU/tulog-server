0839a03483c455e91a9a64735c19f7af7314a9a2
leejucheol <161808249+leejucheol@users.noreply.github.com>
2025-08-20 00:54:20 +0900

    update: search about user

--- api/src/search/search.controller.ts
+++ api/src/search/search.controller.ts
@@ -1,13 +1,18 @@
 import { BadRequestException, Controller, Get, Query } from '@nestjs/common';
 import { SearchResponseDto, SearchService } from './search.service';
+import { Post } from 'src/post/post.entity';
+import { SearchPostDto } from './search.dto';
 
 @Controller('search')
 export class SearchController {
   constructor(private readonly searchService: SearchService) {}
 
   // tag를 입력받으면 태그와 관련된 게시글, 유저 등 정보를 조회
-  @Get('tag')
-  async searchByTag(@Query('q') query: string): Promise<SearchResponseDto> {
+  // 유저 혹은 게시글 정보를 못불러올 수 있으니 null로도 반환가능
+  @Get()
+  async searchByTag(
+    @Query('q') query: string,
+  ): Promise<SearchResponseDto | null> {
     if (!query) {
       throw new BadRequestException('You have to write the message');
     }
