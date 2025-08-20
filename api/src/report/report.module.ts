266726f475757bd59a834d4af9bb7b4c3454d506
leejucheol <161808249+leejucheol@users.noreply.github.com>
2025-08-20 21:12:28 +0900

    update module

--- api/src/report/report.module.ts
+++ api/src/report/report.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ReportService } from './report.service';
 import { ReportController } from './report.controller';
+import { ReportRepository } from './report.repository';
+import { UserModule } from 'src/user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Report } from './report.entity';
 
 @Module({
-  providers: [ReportService],
-  controllers: [ReportController]
+  imports: [TypeOrmModule.forFeature([Report]), UserModule],
+  providers: [ReportService, ReportRepository],
+  controllers: [ReportController],
 })
 export class ReportModule {}
--- api/src/search/search.controller.ts
+++ api/src/search/search.controller.ts
@@ -1,7 +1,7 @@
 import { BadRequestException, Controller, Get, Query } from '@nestjs/common';
-import { SearchResponseDto, SearchService } from './search.service';
+
 import { SearchPostDto } from './search.dto';
-import { SearchService } from './search.service';
+import { SearchResponseDto, SearchService } from './search.service';
 
 @Controller('search')
 export class SearchController {
